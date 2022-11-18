af0bc326938e1a69243a67786c7b0c8ee20d5dd1
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-11-18 16:44:06 -0500

    completed exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,14 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { string myName = "Kassandra Johnson";
+          int myAge = 34;
+          char myGrade = 'A';
+          bool myTruth = true;
+          double myGradePointAvg = 3.87;
+          decimal myChanceOfFailure = 0.0000000000000001m;
+
+            Console.WriteLine($"My name is {myName}, and I am {myAge} years old.  I have always been an {myGrade} student with a ~{myGradePointAvg} grade point average, this is {myTruth}.  Therefore, I believe my chance of failure for becoming a software engineer is {myChanceOfFailure}%.");
         }
     }
 }
